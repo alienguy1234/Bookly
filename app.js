d28c13d47223b33dc632dc3c8b813bfaa32aab99
Student05 <student05@gfrmedia.com>
2017-06-28 11:12:20 -0500

    ellos

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 const test = require('./routes/test');
+var promotion = require('./routes/promotion');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/promotion', promotion);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
